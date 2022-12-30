a23aa2de0a28a941cc0c02b773ccc73f69c6806d
devanshukd04 <devanshukd27@gmail.com>
2022-12-30 14:08:24 +0530

    Implemented suggested changes and did object destructuring

--- backend/controllers/Events/events.js
+++ backend/controllers/Events/events.js
@@ -5,88 +5,125 @@ import Events from '../../models/event.model.js';
 export const getEvents=async (req,res)=>{
     try{
         const events=await Events.find();
-        res.status(200).json({message:"200",events});
+        res.status(200).json({message:"Fetched all events",data:events});
     }   
     catch(e){
-        res.status(404).json({message:"404",data:e.message});
+        res.status(404).json({message:"Failed to fetch events",data:e.message});
     }
 }
 
 //read single event
 export const getEvent=async (req,res)=>{
     try{
-        const body=req.body;
-        const id=body.id;
+        const {id}=req.body;
         const eventDetail=await Events.findById(id);
         if(!eventDetail){
-            res.status(404).json({message:"404",data:"Event with given ID does not exits, Bad Request"});
+            res.status(404).json({message:"Failed to fetch event, event with given ID does not exits, Bad Request"});
         }
         else{
-            res.status(200).json({message:"200",eventDetail});
+            res.status(200).json({message:"Fetched event",data:eventDetail});
         }
     }
     catch(e){
-        res.status(404).json({message:"404",data:e.message});
+        res.status(404).json({message:"Failed to fetch event",data:e.message});
     }
 }
 
 //new event
 export const createEvent=async (req,res)=>{
     if(Object.keys(req.body).length==0){
-        return res.status(400).json({message:"400",data:"Empty Request"});
+        return res.status(400).json({message:"Failed to create event"});
     }
-    console.log(req.body.id);
-    const newEvent=Events(req.body);
-    console.log(newEvent);
+    const {title,venue,schedule,description,duration,date,thumbnail,images,registrationLink,isOnline,registrationFee}=req.body;
+    const newEvent=new Events({
+        title:title,
+        venue:venue,
+        schedule:schedule,
+        description:description,
+        duration:duration,
+        date:date,
+        thumbnail:thumbnail,
+        images:images,
+        registrationLink:registrationLink,
+        isOnline:isOnline,
+        registrationFee:registrationFee
+    });
     //const updated={...newEvent,timestamps:new timeStamp()};
     try{
         await newEvent.save();
-        res.status(201).json({message:"200",newEvent});
+        res.status(201).json({message:"Event Added",data:newEvent});
     }
     catch(e){
-        res.status(404).json({message:"404",data:e.message});
+        res.status(404).json({message:"Failed to create event",data:e.message});
     }
 }
 
 //update event
 export const updateEvent=async(req,res)=>{
     try{
-        const event=req.body;
-        const id=event.id;
+
+        const {id,title,venue,schedule,description,duration,date,thumbnail,images,registrationLink,isOnline,registrationFee}=req.body;
+        const event=new Events({
+            title:title,
+            venue:venue,
+            schedule:schedule,
+            description:description,
+            duration:duration,
+            date:date,
+            thumbnail:thumbnail,
+            images:images,
+            registrationLink:registrationLink,
+            isOnline:isOnline,
+            registrationFee:registrationFee
+        });
+
+
         if(Object.keys(event).length==0){
-            return res.status(400).json({message:"400",data:"Empty Request"});
+            return res.status(400).json({message:"Failed to update event",data:"Empty Request"});
         }
         if(!id){
-            return res.status(400).json({message:"400",data:"Empty Request, Id not provided"});
+            return res.status(400).json({message:"Failed to update event",data:"Empty Request, Id not provided"});
         }
         if(!mongoose.Types.ObjectId.isValid(id)){
-            return res.status(404).json({message:"404",data:"No event with given Id exists"});
+            return res.status(404).json({message:"Failed to update event",data:"No event with given Id exists"});
         }
-        const updatedEvent=await Events.findByIdAndUpdate(id,{...event,id},{new:true});
-        res.status(201).json({message:"200",updatedEvent});
+        const updatedEvent=await Events.findByIdAndUpdate(id,{
+            title:title,
+            venue:venue,
+            schedule:schedule,
+            description:description,
+            duration:duration,
+            date:date,
+            thumbnail:thumbnail,
+            images:images,
+            registrationLink:registrationLink,
+            isOnline:isOnline,
+            registrationFee:registrationFee
+        },{new:true});
+        res.status(201).json({message:"Event Updated",data:updatedEvent});
     }
     catch(e){
-        res.status(404).json({message:"404",data:e.message});
+        res.status(404).json({message:"Failed to update event",data:e.message});
     }
 }
 
 //delete event
 export const deleteEvent=async(req,res)=>{
     try{
-        const id=req.body.id;
+        const {id}=req.body;
         if(Object.keys(req.body).length==0){
-            return res.status(400).json({message:"400",data:"Empty Request"});
+            return res.status(400).json({message:"Failed to delete event",data:"Empty Request"});
         }
         if(!id){
-            return res.status(400).json({message:"400",data:"Empty Request, Id not provided"});
+            return res.status(400).json({message:"Failed to delete event",data:"Empty Request, Id not provided"});
         }
         if(!mongoose.Types.ObjectId.isValid(id)){
-            return res.status(404).json({message:"404",data:"No event with given Id exists"});
+            return res.status(404).json({message:"Failed to delete event",data:"No event with given Id exists"});
         }
         await Events.findByIdAndRemove(id);
-        res.status(200).json({message:"200",data:"Event successfully deleted"});
+        res.status(200).json({message:"Event successfully deleted",data:"Event successfully deleted"});
     }
     catch(e){
-        res.status(404).json({message:"404",data:e.message});
+        res.status(404).json({message:"Failed to delete event",data:e.message});
     }
 }
